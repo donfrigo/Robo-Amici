d280c5f2bdc543b3cc81ac906e7a6c0dc3bc1efe
donfrigo <donfrigo@gmail.com>
2018-09-18 18:13:55 +0200

    manifest.json fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
     return (
         <div className='bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://www.robohash.org/${name}?set=any?size=200x200`}></img>
+            <img alt='robots' src={`https://www.robohash.org/${name}?set=any`}></img>
             <div>
                 <h2>{ name }</h2>
                 <p>{ email }</p>
